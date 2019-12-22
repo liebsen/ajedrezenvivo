94d0c96031ec58a5c7889866fd56533dce98c7b6
martin <telemagico@gmail.com>
2019-12-22 20:19:59 -0300

    Preferences piece themes

--- src/components/Preferences.vue
+++ src/components/Preferences.vue
@@ -22,26 +22,27 @@
         <div class="column">
           <label class="label">Temas</label>
           <div class="field">
-            <input v-model="data.pieces" class="is-checkradio is-success" id="pieces1" type="radio" value="classic" :checked="!data.pieces">
-            <label for="pieces1">Classic</label>
+            <select v-model="data.pieces">
+              <option value="classic">Classic</option>
+              <option value="neo">Neo</option>
+              <option value="wood">Wood</option>
+              <option value="newspaper">Newspaper</option>
+              <option value="maya">Maya</option>
+              <option value="bases">Bases</option>
+              <option value="condal">Condal</option>
+              <option value="modern">Modern</option>
+              <option value="club">Club</option>
+              <option value="neon">Neon</option>
+            </select>
           </div>
-
-          <div class="field">
-            <input v-model="data.pieces" class="is-checkradio is-success" id="pieces2" type="radio" value="neo">
-            <label for="pieces2">Neo</label>
-          </div>
-
-          <div class="field">
-            <input v-model="data.pieces" class="is-checkradio is-success" id="pieces3" type="radio" value="wood">
-            <label for="pieces3">Wood</label>
-          </div>
-
-          <div class="field">
-            <input v-model="data.pieces" class="is-checkradio is-success" id="pieces4" type="radio" value="neon">
-            <label for="pieces4">Neon</label>
+          <div class="board-container">
+            <div :class="boardColor">
+              <div id="board"></div>          
+            </div>
           </div>
         </div>
       </div>
+
       <div class="columns has-text-centered">
         <div class="column">
           <input type="button" @click="submit" class="button is-rounded is-success" :class="{ 'is-loading' : loading }" value="Guardar">
@@ -53,16 +54,26 @@
 
 <script>
 
+  import Chess from 'chess.js'
+  import Chessboard from '../../assets/js/chessboard'
   import snackbar from '../components/Snackbar'
 
   export default {
     name: 'preferences',
+    watch: {
+      'data.pieces': function (val) {
+        this.boardColor = val
+        this.drawBoard()
+      }
+    },
     mounted: function(){
       const saved = localStorage.getItem('player')
       if(saved){
         this.data = JSON.parse(saved)
         this.nick = this.data.code
       }
+
+      this.drawBoard()
     },
     sockets: {
       nick: function (data) {
@@ -79,6 +90,21 @@
       }
     },
     methods: {
+      drawBoard:function(){
+        this.boardEl = document.getElementById('board')
+        this.game = new Chess()
+
+        if(this.data.pieces){
+          this.boardCfg.pieceTheme = '/assets/img/chesspieces/' + this.data.pieces + '/{piece}.png'
+          this.boardColor = this.data.pieces
+        }
+
+        this.board = Chessboard('board', this.boardCfg)      
+
+        $(window).resize(() => {
+          this.board.resize()
+        })
+      },
       submit: function(){
         if(this.nick === this.data.code){
           localStorage.setItem('player', JSON.stringify(this.data))
@@ -95,8 +121,17 @@
     },
     data () {
       return {
+        boardCfg: {
+          showErrors:true,
+          position: 'start',
+          draggable: false,
+          pieceTheme:'/assets/img/chesspieces/classic/{piece}.png'
+        },
         data:{},
         nick:null,
+        boardColor:null,
+        boardEl:null,
+        game:null,
         loading: false
       }
     }
