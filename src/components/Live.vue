5472b8515723ff49d12eced0243406647bf3e72b
martin <telemgico@gmail.com>
2020-02-25 14:42:40 -0300

    search pgn explicit

--- src/components/Live.vue
+++ src/components/Live.vue
@@ -132,7 +132,7 @@
           }
           this.pages = pages
           this.$root.loading = false
-          axios.post(this.$root.endpoint + '/eco/search', {query: this.query}).then((res2) => {
+          axios.post(this.$root.endpoint + '/eco/search/pgn', {pgn: this.query}).then((res2) => {
             if(res2.data.games[0]){
               t.eco = res2.data.games[0]
             }
