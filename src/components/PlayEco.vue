90e589cb0217ee62fee1c0e05fb081eddfcb5da9
martin <telemgico@gmail.com>
2020-03-01 09:59:18 -0300

    hide chart mobile

--- src/components/PlayEco.vue
+++ src/components/PlayEco.vue
@@ -63,7 +63,7 @@
                   </div>
                 </div>
               </div>
-              <div class="columns">
+              <div class="columns is-hidden-mobile">
                 <div class="movesTableContainer preservefilter">
                   <div class="movesTable">
                     <div class="moveRow" v-for="(move,index) in pgnIndex">
