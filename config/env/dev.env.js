be3f3170c3e3db16d30bcf47855119909ee274aa
martin <telemgico@gmail.com>
2020-03-07 10:44:10 -0300

    search fix

--- config/env/dev.env.js
+++ config/env/dev.env.js
@@ -1,4 +1,5 @@
 module.exports = {
     NODE_ENV: 'development',
-    ENDPOINT: 'http://localhost:4000'
+    // ENDPOINT: 'http://localhost:4000'
+    ENDPOINT: 'https://ajedrezenvivoapi.herokuapp.com'
 }
